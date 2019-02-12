c8de410eddb48e70a1ece837ccbe278c848dc5d0
vibecode <rumashkin@gmail.com>
2019-02-12 21:03:33 +0100

    migrate to react

---
+++ src/constants/links.js
@@ -0,0 +1,25 @@
+export const reactProjects = [
+  {
+    name: 'Pixel Expert Quize Game',
+    link: 'https://pixel-expert.herokuapp.com',
+    repo: 'https://github.com/vibecode/pixel-expert'
+  },
+  {
+    name: 'Online shop boilerplate',
+    link: 'https://react-e-shop.herokuapp.com',
+    repo: 'https://github.com/vibecode/react-e-shop'
+  },
+  {
+    name: `Asana style kanban app with drag'n'drop feature`,
+    link: 'https://vibecode.github.io/kanban-redux',
+    repo: 'https://github.com/vibecode/kanban-redux'
+  }
+]
+
+export const htmlProjects = [
+  {
+    name: 'Ice cream shop Gllasy',
+    link: 'https://vibecode.github.io/icecream',
+    repo: 'https://github.com/vibecode/icecream'
+  }
+]
