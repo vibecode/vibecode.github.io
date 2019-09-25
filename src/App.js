70a06ecc512482d513d5a250dc48fdf672c833fe
vibecode <rumashkin@gmail.com>
2019-09-25 21:55:36 +0200

    setup spring animation

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import styles from './App.module.scss'
 import { reactProjects, htmlProjects } from './constants/links'
 import Blob from './Blob'
 
-class App extends Component {
-  renderProjects(projects) {
+function App() {
+  const renderProjects = projects => {
     return projects.map(({ name, link, repo }) => {
       return (
         <li key={name}>
@@ -25,35 +25,33 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <main className={styles.main}>
-        <div className={styles.blob_container}>
-          <h1 className={styles.hi}>{'<Hi\u00A0/>'}</h1>
-          <Blob />
-        </div>
-        <div className={styles.container}>
-          <p className={styles.info}>
-            My name is <strong>Stanislav Ovcharov</strong>, I'm Javascript
-            developer and graphic/sound designer currently based in Amsterdam,
-            Netherlands. I love making good looking apps, especially with React
-            and Node. Here are some of the projects I've done which helped me to
-            acquire the knowlege about how things works in the modern
-            web-development world:
-          </p>
-        </div>
-        <Collapse initOpen={false}>
-          <div className={styles.container_links}>
-            <h3>React projects:</h3>
-            <ul>{this.renderProjects(reactProjects)}</ul>
+  return (
+    <main className={styles.main}>
+      <div className={styles.blob_container}>
+        <h1 className={styles.hi}>{'<Hi\u00A0/>'}</h1>
+        <Blob />
+      </div>
+      <div className={styles.container}>
+        <p className={styles.info}>
+          My name is <strong>Stanislav Ovcharov</strong>, I'm Javascript
+          developer and graphic/sound designer currently based in Amsterdam,
+          Netherlands. I love making good looking apps, especially with React
+          and Node. Here are some of the projects I've done which helped me to
+          acquire the knowlege about how things works in the modern
+          web-development world:
+        </p>
+      </div>
+      <Collapse initOpen={false}>
+        <div className={styles.container_links}>
+          <h3>React projects:</h3>
+          <ul>{renderProjects(reactProjects)}</ul>
 
-            <h3>Html projects:</h3>
-            <ul>{this.renderProjects(htmlProjects)}</ul>
-          </div>
-        </Collapse>
-      </main>
-    )
-  }
+          <h3>Html projects:</h3>
+          <ul>{renderProjects(htmlProjects)}</ul>
+        </div>
+      </Collapse>
+    </main>
+  )
 }
 
 export default App
--- src/Collapse.js
+++ src/Collapse.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react'
 import styles from './Collapse.module.scss'
+import { useSpring, animated } from 'react-spring'
 
 const Collapse = ({ children, initOpen }) => {
-  const [collapse, setOpen] = useState(initOpen)
+  const [show, setOpen] = useState(initOpen)
+  const anim = useSpring({ opacity: show ? 1 : 0 })
 
   return (
     <div className={styles.container}>
-      <button className={styles.button} onClick={() => setOpen(open => !open)}>
+      <button className={styles.button} onClick={() => setOpen(show => !show)}>
         Open
       </button>
-      <div className={collapse ? styles.content : styles.content_closed}>
-        {children}
-      </div>
+      {show && (
+        <animated.div className={styles.content} style={anim}>
+          {children}
+        </animated.div>
+      )}
     </div>
   )
 }
