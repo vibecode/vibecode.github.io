b3c8e14dda591e8e91fb6b805ad075e22d4e794d
vibecode <rumashkin@gmail.com>
2019-09-27 16:42:48 +0200

    Updates

---
+++ precache-manifest.506f09a01aa107c283631c86f66976f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f107f76df773a1217caac619c21887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4553ecd8066ec6ad7b2",
+    "url": "/static/css/main.38865662.chunk.css"
+  },
+  {
+    "revision": "80f582a7bc71fd637c1e",
+    "url": "/static/js/2.c710955e.chunk.js"
+  },
+  {
+    "revision": "b4553ecd8066ec6ad7b2",
+    "url": "/static/js/main.3cee550f.chunk.js"
+  },
+  {
+    "revision": "9dccccd3d3820f571426",
+    "url": "/static/js/runtime-main.0238449d.js"
+  }
+]);
