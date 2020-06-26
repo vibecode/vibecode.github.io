52e36a4211976550e4913bf04212dbc9fa73a67c
vibecode <rumashkin@gmail.com>
2020-06-26 17:27:06 +0200

    hide page src link

--- src/constants/links.js
+++ src/constants/links.js
@@ -2,38 +2,38 @@ export const reactProjects = [
   {
     name: 'Online store feedback quize (Typeform template clone)',
     link: 'https://vibecode.github.io/react-feedback',
-    repo: 'https://github.com/vibecode/react-feedback'
+    repo: 'https://github.com/vibecode/react-feedback',
   },
   {
     name: 'Pixel Expert Quize Game',
     link: 'https://pixel-expert.herokuapp.com',
-    repo: 'https://github.com/vibecode/pixel-expert'
+    repo: 'https://github.com/vibecode/pixel-expert',
   },
   {
     name: 'Online shop boilerplate',
     link: 'https://react-e-shop.herokuapp.com',
-    repo: 'https://github.com/vibecode/react-e-shop'
+    repo: 'https://github.com/vibecode/react-e-shop',
   },
   {
     name: `Asana style kanban app with drag'n'drop feature`,
     link: 'https://vibecode.github.io/kanban-redux',
-    repo: 'https://github.com/vibecode/kanban-redux'
+    repo: 'https://github.com/vibecode/kanban-redux',
   },
   {
     name: `Simple snake game made with react and typescript`,
     link: 'https://vibecode.github.io/snake-game',
-    repo: 'https://github.com/vibecode/snake-game'
+    repo: 'https://github.com/vibecode/snake-game',
   },
-  {
-    name: 'This page source code',
-    link: 'https://github.com/vibecode/vibecode.github.io/tree/src'
-  }
+  // {
+  //   name: 'This page source code',
+  //   link: 'https://github.com/vibecode/vibecode.github.io/tree/src'
+  // }
 ]
 
 export const htmlProjects = [
   {
     name: 'Ice cream shop Gllasy',
     link: 'https://vibecode.github.io/icecream',
-    repo: 'https://github.com/vibecode/icecream'
-  }
+    repo: 'https://github.com/vibecode/icecream',
+  },
 ]
